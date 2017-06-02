f744359ea861a0e7218891fb7c19464f416c9b5f
Matheus Clemente <matheus_klementt@hotmail.com>
2017-06-02 15:12:13 -0300

    Classe TratarTelefone

---
+++ TratamentoDeTelefone/TratarTelefone.swift
@@ -0,0 +1,9 @@
+//
+//  TratarTelefone.swift
+//  TratamentoDeTelefone
+//
+//  Created by Matheus Azevedo on 02/06/17.
+//  Copyright © 2017 Matheus Azevedo. All rights reserved.
+//
+
+import Foundation
