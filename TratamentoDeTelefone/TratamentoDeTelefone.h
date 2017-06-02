dddc7a900afa64d8b0fe287ccbc61c3120096c33
Matheus Clemente <matheus_klementt@hotmail.com>
2017-06-02 14:56:16 -0300

    Commit inicial

---
+++ TratamentoDeTelefone/TratamentoDeTelefone.h
@@ -0,0 +1,19 @@
+//
+//  TratamentoDeTelefone.h
+//  TratamentoDeTelefone
+//
+//  Created by Matheus Azevedo on 02/06/17.
+//  Copyright © 2017 Matheus Azevedo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TratamentoDeTelefone.
+FOUNDATION_EXPORT double TratamentoDeTelefoneVersionNumber;
+
+//! Project version string for TratamentoDeTelefone.
+FOUNDATION_EXPORT const unsigned char TratamentoDeTelefoneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TratamentoDeTelefone/PublicHeader.h>
+
+
