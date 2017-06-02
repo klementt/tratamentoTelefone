12082e745d56e83cf94fdb8c787f1bd4915de1ef
Maisa <maisa.milena@gmail.com>
2017-06-02 17:37:30 -0300

    Voltando o código porque deu ruim tentando passar a framework pro outro projeto

--- TratamentoDeTelefone/TratarTelefone.swift
+++ TratamentoDeTelefone/TratarTelefone.swift
@@ -18,4 +18,40 @@ public class TratarTelefone: NSObject {
     
     
     
+    
+    
+    
+    
+    
+    /**
+     Reponsável por receber uma string e fazê-la adequar-se ao padrão de telefone (__) _____-____
+     - parameter telefone : aksjdhad
+     */
+    func formatarTelefone(telefone: String) -> String {
+        let qtdCaracteres = telefone.characters.count
+        var novoTelefone: String
+        
+        if qtdCaracteres != 11 || qtdCaracteres != 10 {
+            return "A quantidade de caracteres no telefone está inválida. É preciso ter 11 ou 10 números"
+        }
+        
+        // verifica se contém apenas números
+        if let numeros: Int = Int(telefone)  {
+            novoTelefone = telefone.replacingOccurrences(of: " ", with: "") //tira espaço em branco
+            
+            // Usar essa expresão: ^\([1-9]{2}\) [2-9][0-9]{3,4}\-[0-9]{4}$
+            var regex: String = "^\\([1-9]{2}\\) [2-9][0-9]{3,4}\\-[0-9]{4}$"
+            
+            if let range = novoTelefone.range(of:regex, options: .regularExpression) {
+                let result = novoTelefone.substring(with:range)
+            }
+            return novoTelefone
+        } else {
+            return "O número deve conter somente números"
+        }
+    }
+    
+    
+    
+    
 }
